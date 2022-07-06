a8e53f5882d2012d003b81471edcce6d2f105b07
Leo Mendez <leomenbel93@gmail.com>
2022-07-06 08:21:57 -0700

    feat: added hamburger menu

---
+++ src/components/HamburgerMenu/HamburgerMenu.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+type Props = {
+  children: React.ReactNode;
+  navOpen: boolean;
+  setNavOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function HamburgerMenu({ children, navOpen, setNavOpen }: Props) {
+  return (
+    <>
+      <div className={`w-full h-[90%] absolute top-16 left-0 ${navOpen ? ' backdrop-blur-sm z-10' : ' -z-10'}`} />
+      <div
+        className={`absolute text-lg transition-all duration-300 top-16 right-0 sm:relative 
+            sm:visible sm:top-0 sm:w-auto sm:gap-6 h-5/6 sm:h-auto
+           dark:bg-asce-bg-dark bg-asce-bg px-8 py-6 sm:shadow-none
+          ${
+            navOpen
+              ? ' shadow-md dark:shadow-asce-primary-dark w-2/3 z-10 visible'
+              : 'w-0 z-0 p-0 dark:bg-transparent bg-transparent'
+          }`}
+      >
+        {children}
+      </div>
+      <button className="sm:hidden flex flex-col gap-1" onClick={() => setNavOpen((prev) => !prev)}>
+        <span
+          className={`w-4 h-0.5 bg-asce-font dark:bg-asce-font-dark transition-all duration-200
+            ${navOpen && 'rotate-45 translate-y-1'}`}
+        />
+        <span
+          className={`w-4 h-0.5 bg-asce-font dark:bg-asce-font-dark transition-all duration-200
+            ${navOpen && 'hidden'}`}
+        />
+        <span
+          className={`w-4 h-0.5 bg-asce-font dark:bg-asce-font-dark transition-all duration-200
+            ${navOpen && '-rotate-45 -translate-y-0.5'}`}
+        />
+      </button>
+    </>
+  );
+}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,15 @@
 import { useTheme } from 'next-themes';
 import Link from 'next/link';
 import { MdDarkMode, MdLightMode } from 'react-icons/md';
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import Image from 'next/image';
+import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
 
 type Props = {};
 
 export default function Navbar({}: Props) {
   const { theme, setTheme } = useTheme();
+  const [navOpen, setNavOpen] = useState(false);
 
   const icon = useMemo(() => (theme === 'dark' ? <MdDarkMode /> : <MdLightMode />), [theme]);
 
@@ -21,20 +23,43 @@ export default function Navbar({}: Props) {
           </a>
         </Link>
       </div>
-      <div className="flex justify-between items-center gap-8 text-lg">
-        <Link href="/projects">
-          <a className="underline-animation font-medium">Projects</a>
-        </Link>
-        <Link href="/">
-          <a className="underline-animation font-medium">Home</a>
-        </Link>
-        <button
-          className="shadow-sm border-asce-font dark:border-asce-font-dark border-2 rounded-full p-2 hover:opacity-70 text-sm"
-          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      <HamburgerMenu navOpen={navOpen} setNavOpen={setNavOpen}>
+        <div
+          className={`flex-col text-center sm:flex-row sm:visible sm:top-0 sm:w-auto sm:h-auto sm:flex
+            gap-6 sm:gap-0
+            ${navOpen ? ' flex items-center' : 'w-0 hidden'}`}
         >
-          {icon}
-        </button>
-      </div>
+          <Link href="/projects">
+            <a
+              className={
+                'underline-animation font-medium border-b w-full sm:border-b-0 sm:border-r sm:px-6 sm:pb-0 pb-6 ' +
+                'border-asce-font dark:border-asce-font-dark'
+              }
+            >
+              Projects
+            </a>
+          </Link>
+          <Link href="/">
+            <a
+              className={
+                'underline-animation font-medium border-b w-full sm:border-b-0 sm:border-r sm:px-8 sm:pb-0 pb-6 ' +
+                'border-asce-font dark:border-asce-font-dark'
+              }
+            >
+              Home
+            </a>
+          </Link>
+          <button
+            className={
+              'shadow-sm border-asce-font dark:border-asce-font-dark border-2 rounded-full p-2 ml-6 ' +
+              'hover:opacity-70 text-sm flex justify-center items-center gap-2'
+            }
+            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+          >
+            {icon}
+          </button>
+        </div>
+      </HamburgerMenu>
     </div>
   );
 }
