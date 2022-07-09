96d29be9adece6c324a515f2762ceb8f57ec117b
Leo Mendez <leomenbel93@gmail.com>
2022-07-09 11:26:13 -0700

    fix: fixed footer positioning

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -2,9 +2,12 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html className="h-full">
       <Head />
-      <body className="bg-asce-bg dark:bg-asce-bg-dark text-asce-font dark:text-asce-font-dark transition-all">
+      <body
+        className="bg-asce-bg dark:bg-asce-bg-dark text-asce-font dark:text-asce-font-dark transition-all
+        h-full"
+      >
         <Main />
         <NextScript />
       </body>
