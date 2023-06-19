556f59bafe4e407303d1a4a8d753654609fb0606
Leo Mendez <leomenbel93@gmail.com>
2023-06-19 12:53:33 -0700

    added animations to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -17,16 +17,25 @@ export default function Page() {
         >
           Get In Touch
         </motion.h1>
-        <div className="text-slate-400 flex gap-16 flex-col items-center">
-          <motion.p className="text-xl" >Feel free to reach me via my socials below</motion.p>
-          <motion.p className="flex gap-1 flex-wrap items-center justify-center">
+        <motion.div
+          className="text-slate-400 flex gap-16 flex-col items-center"
+          initial={{ opacity: 0, x: -400 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+        >
+          <p className="text-xl">Feel free to reach me via my socials below</p>
+          <p className="flex gap-1 flex-wrap items-center justify-center">
             And to give credit where it is due. Please visit{' '}
-            <a href="https://brittanychiang.com/" target="_blank" className="text-teal-600 flex items-center gap-1 hover:font-bold">
+            <a
+              href="https://brittanychiang.com/"
+              target="_blank"
+              className="text-teal-600 flex items-center gap-1 hover:font-bold"
+            >
               brittanychiang.com <BiLinkExternal />
             </a>{' '}
             I used her site as inspiration for this one.
-          </motion.p>
-        </div>
+          </p>
+        </motion.div>
       </div>
     </main>
   );
