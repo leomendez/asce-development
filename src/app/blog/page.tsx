4694959f6680f297147dda0043176836ca285013
Leo Mendez <leomenbel93@gmail.com>
2025-09-29 21:20:43 -0700

    fixed styling on blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,11 +3,11 @@ export default function Page() {
     <main className="h-full">
       <div className="p-8 md:p-36 flex flex-col h-full items-start overflow-x-hidden">
         <h1 className="text-4xl font-bold">Blog</h1>
-        <div className="text-slate-400 flex gap-16 flex-col items-center md:mt-20 mt-8 w-full">
-          <p className="text-xl text-center">
+        <div className="text-slate-400 flex gap-16 flex-col items-start md:mt-20 mt-8 w-full">
+          <p className="text-xl ">
             Technical insights and thoughts on software engineering coming soon.
           </p>
-          <p className="text-center text-sm">
+          <p className="text-sm">
             In the meantime, feel free to check out my projects or get in touch.
           </p>
         </div>
